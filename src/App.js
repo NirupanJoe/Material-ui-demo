be7c232b2628feeecef013a5934e00bc77348215
nirupan <nirupanjoej@gmail.com>
2021-07-03 12:25:23 +0530

    matetrial-ui button

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import { Button } from '@material-ui/core';
 const App = () =>
 	<div className="App">
 		<Button color="primary">Hello World</Button>
+		<Button variant="contained">Default</Button>
+		<Button variant="contained" color="primary">
+			Primary
+		</Button>
+		<Button variant="contained" color="secondary">
+			Secondary
+		</Button>
+		<Button variant="contained" disabled="disabled">
+			Disabled
+		</Button>
+		<Button variant="contained" color="primary" href="#contained-buttons">
+			Link
+		</Button>
 	</div>;
 
 export default App;
