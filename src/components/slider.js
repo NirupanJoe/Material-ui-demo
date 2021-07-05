790cb45f2f7085f928676eec9041ec74eabcbd28
nirupan <nirupanjoej@gmail.com>
2021-07-05 12:52:15 +0530

    introduced a slider icon

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography, Slider } from '@material-ui/core';
+import FormatSizeSharpIcon from '@material-ui/icons/FormatSizeSharp';
 import context from '../core/context';
 
 const VolumeSlider = () =>
@@ -7,6 +8,7 @@ const VolumeSlider = () =>
 		<Typography id="continuous-slider">
 			FontSize
 		</Typography>
+		<FormatSizeSharpIcon/>
 		<Slider
 			value={ context.state.fontSize }
 			onChange={ (dummy, newValue) => context.actions
