77bb4a96fa5d79de1b8272837053dd2802f9a3a3
Nirupan Joe <nirupanjoej@gmail.com>
2021-07-15 12:12:31 +0000

    introduced a style

--- src/App.js
+++ src/App.js
@@ -1,31 +1,10 @@
 import { React } from 'react';
 import './App.scss';
-import ButtonInput from './components/button';
-import RadioButton from './components/radioButton';
-import SelectInput from './components/selectInput';
-import FontSlider from './components/slider';
-import SwitchButton from './components/switchButton';
-import context from './core/context';
-
-const theme = () => `App ${ context.state.theme ? 'dark' : 'lite' }`;
-
-const StyleFont = () => ({
-	fontSize: `${ context.state.fontSize }px`,
-});
+import Style from './components/style';
 
 const App = () =>
-	<div
-		style={ StyleFont() }
-		className={ theme() }
-	>
-		<div className="container">
-			<div>{ButtonInput()}</div>
-			<div>{SelectInput()}</div>
-			<div>{context.state.text}</div>
-			<div>{SwitchButton()}</div>
-			<div>{RadioButton()}</div>
-			<div>{FontSlider()}</div>
-		</div>
+	<div>
+		{Style()}
 	</div>;
 
 export default App;
---
+++ src/components/style/index.js
@@ -0,0 +1,8 @@
+import { Box } from '@material-ui/core';
+import { React } from 'react';
+import ButtonStyle from './button';
+
+const Style = () =>
+	<Box>{ButtonStyle()}</Box>;
+
+export default Style;
